5f0039d35242dcc5ac9d376710d380d921576eeb
notbobutah <rrmackay@gmail.com>
2023-02-24 17:38:58 -0700

    ConfigMap setup for initdb

---
+++ database/data/init_db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+#   Update pg_hba_file_rules set address='0.0.0.0' WHERE type = 'host' and address = '127.0.0.1';
+# sed -i'.BACKUP' 's/127.0.0.1/0.0.0.0/g' /data/db/pg_hba.conf
+#   SELECT pg_reload_conf();
+
+echo 'Initializing database for Thingsboard and spotlight'
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE USER $APP_DB_USER WITH PASSWORD $APP_DB_PASS;
+  CREATE DATABASE $APP_DB_NAME;
+  GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO $APP_DB_USER;
+  ALTER USER $APP_DB_USER with SUPERUSER;
+  \connect $APP_DB_NAME $APP_DB_USER
+  SELECT * from pg_hba_file_rules WHERE type = 'host' and address = '127.0.0.1';
+EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -a -f ../be/src/data/spotlight.data.sql <<-EOSQL
+EOSQL
---
+++ database/data/spotlight.ddl.sql
@@ -0,0 +1,54 @@
+CREATE SEQUENCE IF NOT EXISTS  nodes_id_seq START 100;
+CREATE SEQUENCE IF NOT EXISTS connectors_id_seq START 100;
+CREATE SEQUENCE IF NOT EXISTS diagram_id_seq START 100;
+ 
+-- Table: public.nodes
+CREATE TABLE IF NOT EXISTS nodes
+(
+    id integer NOT NULL DEFAULT nextval('nodes_id_seq'::regclass),
+    nodebody json,
+    "offsetX" bigint,
+    "offsetY" bigint,
+    CONSTRAINT nodes_pkey PRIMARY KEY (id)
+);
+
+
+ALTER TABLE nodes
+    OWNER to postgres;
+
+COMMENT ON TABLE nodes
+    IS 'storage for flow chart nodes';
+
+ -- Table: public.connectors
+
+CREATE TABLE IF NOT EXISTS connectors
+(
+    id integer NOT NULL DEFAULT nextval('connectors_id_seq'::regclass),
+    connectorbody json,
+    sourcenode bigint,
+    targetnode bigint,
+    CONSTRAINT connectors_pkey PRIMARY KEY (id)
+);
+
+ALTER TABLE connectors
+    OWNER to postgres;
+
+COMMENT ON TABLE connectors
+    IS 'storage for connectors in the flow chart';   
+
+    -- Table: public.diagrams
+CREATE TABLE IF NOT EXISTS diagrams
+(
+    id integer NOT NULL DEFAULT nextval('diagram_id_seq'::regclass),
+    dname varchar(256) ,
+    dbody json,
+    CONSTRAINT diagrams_pkey PRIMARY KEY (id)
+);
+
+
+ALTER TABLE diagrams
+    OWNER to postgres;
+
+COMMENT ON TABLE diagrams
+    IS 'storage for flow chart diagrams';
+ 
