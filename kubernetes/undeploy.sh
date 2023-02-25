7b607772b638147d3616f45e6e8e9a3b203abb4e
notbobutah <rrmackay@gmail.com>
2023-02-25 09:15:44 -0700

    Continued database startup work

--- kubernetes/undeploy.sh
+++ kubernetes/undeploy.sh
@@ -1,4 +1,4 @@
-kubectl delete -n spotlight -f db-service.yaml,db-deployment.yaml,db-claim1-persistentvolumeclaim.yaml,db-data-persistentvolumeclaim.yaml
+kubectl delete -n spotlight -f db-service.yaml,db-deployment.yaml,db-pg-data-persistentvolume.yaml
 kubectl delete -n spotlight -f spotlight-fe-service.yaml,spotlight-fe-deployment.yaml
 kubectl delete -n spotlight -f spotlight-be-claim0-persistentvolumeclaim.yaml,spotlight-be-claim1-persistentvolumeclaim.yaml,spotlight-be-service.yaml,spotlight-be-deployment.yaml
 kubectl delete -n spotlight -f tb-data-persistentvolumeclaim.yaml,tb-deployment.yaml,tb-logs-persistentvolumeclaim.yaml,tb-service.yaml
