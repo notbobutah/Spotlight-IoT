3a8739b61a676c7291e386ffa7a595c289ac7ed4
notbobutah <rrmackay@gmail.com>
2023-02-26 13:58:05 -0700

    More container startup cleanup

--- be/start-be.sh
+++ be/start-be.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+pg_uri="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER:5432/postgres"
+# make sure pg is ready to accept connections
+until pg_isready -h $POSTGRES_SERVER -p 5432 -U postgres
+do
+  echo "Waiting for postgres at: $pg_uri"
+  sleep 2;
+done
+
 npx sequelize db:migrate
 npx sequelize db:seed:all
 
